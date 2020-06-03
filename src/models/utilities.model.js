b770607b0f525a8bc0cfa2ec0c1d52ec157bbef8
hanspetercastellar <hanspeter1512@gmail.com>
2020-06-03 10:33:19 -0500

    agregando modelo de plan unico de cuentas

---
+++ src/models/utilities.model.js
@@ -0,0 +1,26 @@
+/**
+ *  @date 01/06/2020
+ *  @author  Simplapp - Hans Peter Castellar
+ *  @description  Modelos utilitarios como lista de ciudades, datos aislados a la logica del negocio
+ * 
+ */
+
+const { Schema, model } = require('mongoose');
+
+const CiudadesSchema = new Schema({
+	id: { unique: true, type: Number },
+	departamento: { type: String },
+	ciudades: [{ type: String }],
+});
+
+const  PucSchema = new Schema({
+	Codigo: { unique: true, type: Number },
+	departamento: { type: String },
+	ciudades: [{ type: String }],
+});
+
+module.exports = {
+                  CiudadModel:  model('colombia', CiudadesSchema),
+                  PucModel:  model('plan_cuentas', PucSchema)
+}   
+
--- src/routes/resources.routes.js
+++ src/routes/resources.routes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 //Used models
-const colombiaModel = require('../models/ciudades.model');
+const {CiudadModel, PucModel} = require('../models/utilities.model');
 
 router.get('/departamentos', async (req, res) => {
+	console.log(CiudadModel)
 	try {
-		let departamentos = await colombiaModel.find(
+		let departamentos = await CiudadModel.find(
 			{},
 			{ departamento: 1, id: 1 }
 		);
@@ -24,7 +25,7 @@ router.get('/ciudades', async (req, res) => {
 	try {
 		const { id } = req.query;
 
-		let data = await colombiaModel.find({ id }, { ciudades: 1 });
+		let data = await CiudadModel.find({ id }, { ciudades: 1 });
 		res.status(201).json({ data: data[0].ciudades, status: 'done' });
 	} catch (error) {
 		res.status(500).json({
@@ -34,5 +35,33 @@ router.get('/ciudades', async (req, res) => {
 		});
 	}
 });
+
+router.get('/puc', async (req, res) => {
+	
+ const { id } = req.query.id !== undefined ? req.query : false;
+	
+	 try {
+		 var resp = {}
+		 if( id !== undefined) {
+			
+			 	 resp = await PucModel.find({Codigo: id}) 
+		 }else {
+			  resp = await PucModel.find({}, null, {skip: 10}).limit(50)
+		 }
+	
+
+		res.status(201).json({ data: resp, status: 'done' });
+
+		}catch(error){
+			res.status(500).json({
+				error,
+				message: 'Error con La base de datos',
+				status: 'fail',
+			});
+			}
+
+});
+
+
 
 module.exports = router;
